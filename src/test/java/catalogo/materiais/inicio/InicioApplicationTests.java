d8b9d579edb152d77f637ba428a36dd2e2ed4a88
gilneto <gil2alves@gmail.com>
2022-01-24 14:39:05 -0300

    commit inicial

---
+++ src/test/java/catalogo/materiais/inicio/InicioApplicationTests.java
@@ -0,0 +1,13 @@
+package catalogo.materiais.inicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
